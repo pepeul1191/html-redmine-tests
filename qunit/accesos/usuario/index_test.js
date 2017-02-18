943079119499925dcaa64301c06e056fbed83608
Pepe Valdivia <jvaldivia@softweb.pe>
2017-02-18 11:10:16 -0500

    Comenzando pruebas del listado de usuarios

--- qunit/accesos/usuario/index_test.js
+++ qunit/accesos/usuario/index_test.js
@@ -26,39 +26,66 @@ QUnit.test( "La tabla debe redireccionar a una nueva vista para agrear un usuari
 	});
 });
 
-QUnit.test( "La tabla debe mostrar un modal para ver los registros de ingreso al sistema del usuario", function( assert ) {
-  	assert.ok( 1 == "1", "Click artificio" );
-
-  	var tbody = $(this.id_tabla).children().eq(1);
+function verAccesosTest(id_tabla){
+  	var tbody = $(id_tabla).children().eq(1);
   	//var filas = $(this.id_tabla).children(0).children().eq(1).children().last().children().eq(3);
   	var numero_filas = tbody.children().length;
 
   	for(var k = numero_filas; k > 0; k = k - 1){
   		var btn_ver_accesos = tbody.children().eq(k - 1).children().last().children().eq(0);
-
-  		btn_ver_accesos.click();
-
-	  	btn_ver_accesos.on( "click", function() {
-	   	 	$.ajax({
-			    type: "GET",
-			    url: BASE_URL + "accesos/usuario/ver_accesos",
-			    data: '',
-			    success: function(data,status,xhr){
-
-			    },
-			    error: function(xhr, status, error){
-			       	
-			    },
-			    complete: function(xhr){
-			    	var status = xhr.status;
-			    	assert.equal(status, 200, "Debe cargar un modal con la lista de accesos del usuario al sistema");
-			    }
+  		console.log(btn_ver_accesos);
+  		
+  		btn_ver_accesos.click(function() {
+    		$.ajax({
+				type: "GET",
+				url: BASE_URL + "accesos/usuario/ver_accesos",
+				data: '',
+				success: function(data,status,xhr){
+					var status = xhr.status;
+					QUnit.test( "La tabla debe mostrar un modal para ver los registros de ingreso al sistema del usuario", function( assert ) {
+				   	assert.equal(status, 200, "Debe cargar un modal con la lista de accesos del usuario al sistema");
+				});
+				},
+				error: function(xhr, status, error){
+				},
+				complete: function(xhr){
+					console.log(tablaAccesos);
+				}
 			});
-	   	 	return false;
-	  	});
+		});
 
-	  	$("#modal-container").empty();
-	  	$(".modal-backdrop").remove();
+  		btn_ver_accesos.trigger("click");
+
+  		$("#modal-container").click();
+		$(".modal-backdrop").remove();
   	}
-  	
+};
+
+verAccesosTest("#tablaUsuarios");
+
+
+$(".mootools").click(function(event){
+	/*
+	$.ajax({
+		type: "GET",
+		url: BASE_URL + "accesos/usuario/ver_accesos",
+		data: '',
+			success: function(data,status,xhr){
+				var status = xhr.status;
+				QUnit.test( "La tabla debe mostrar un modal para ver los registros de ingreso al sistema del usuario", function( assert ) {
+			   		assert.equal(status, 200, "Debe cargar un modal con la lista de accesos del usuario al sistema");
+			   	});
+			},
+			error: function(xhr, status, error){
+			},
+			complete: function(xhr){
+				console.log(tablaAccesos);
+			}
+		});
+
+	$("#modal-container").click();
+	$(".modal-backdrop").remove();
+
+	return false;
+	*/
 });
